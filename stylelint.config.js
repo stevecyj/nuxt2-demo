26b583bef574198e18411a7ecac4306ecd464a2f
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-01 11:57:32 +0800

    install, set tailwindcss

--- stylelint.config.js
+++ stylelint.config.js
@@ -9,12 +9,18 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    // 'at-rule-no-unknown': [
-    //   true,
-    //   {
-    //     ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen'],
-    //   },
-    // ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'tailwind',
+          'apply',
+          'variants',
+          'responsive',
+          'screen',
+        ],
+      },
+    ],
     // 'declaration-block-trailing-semicolon': null,
     // 'no-descending-specificity': null,
     // 'no-duplicate-selectors': null,
